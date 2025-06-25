d58614f3bb3aa12325d6e27b07b87b8646deec18
Ivan Pedrazas <ipedrazas@gmail.com>
2025-06-25 16:29:52 +0100

    chore: fix catalog add to make it work with local files (#18)
    * chore: fix catalog add to make it work with local files
    
    Signed-off-by: Ivan Pedrazas <ivan.pedrazas@docker.com>
    
    * chore: clean up to make linting happy again
    
    Signed-off-by: Ivan Pedrazas <ivan.pedrazas@docker.com>
    
    ---------
    
    Signed-off-by: Ivan Pedrazas <ivan.pedrazas@docker.com>

--- cmd/docker-mcp/catalog/add.go
+++ cmd/docker-mcp/catalog/add.go
@@ -3,7 +3,7 @@ package catalog
 import (
 	"errors"
 	"fmt"
-	"strings"
+	"os"
 
 	"github.com/spf13/cobra"
 
@@ -17,14 +17,11 @@ type addOpts struct {
 func newAddCommand() *cobra.Command {
 	opts := &addOpts{}
 	cmd := &cobra.Command{
-		Use:   "add <catalog> <source-catalog>/<server-name>",
+		Use:   "add <catalog> <server-name> <catalog-file>",
 		Short: "Add a server to your catalog",
-		Args:  cobra.ExactArgs(2),
+		Args:  cobra.ExactArgs(3),
 		RunE: func(_ *cobra.Command, args []string) error {
-			parsedArgs, err := parseAddArgs(args[0], args[1])
-			if err != nil {
-				return err
-			}
+			parsedArgs := parseAddArgs(args[0], args[1], args[2])
 			if err := validateArgs(*parsedArgs); err != nil {
 				return err
 			}
@@ -43,16 +40,12 @@ type ParsedAddArgs struct {
 	SeverName string
 }
 
-func parseAddArgs(dst, src string) (*ParsedAddArgs, error) {
-	srcParts := strings.Split(src, "/")
-	if len(srcParts) != 2 {
-		return nil, fmt.Errorf("cannot parse %s: expected format <source-catalog>/<server-name>", src)
-	}
+func parseAddArgs(dst, src, catalogFile string) *ParsedAddArgs {
 	return &ParsedAddArgs{
-		Src:       srcParts[0],
+		Src:       catalogFile,
 		Dst:       dst,
-		SeverName: srcParts[1],
-	}, nil
+		SeverName: src,
+	}
 }
 
 func validateArgs(args ParsedAddArgs) error {
@@ -60,16 +53,20 @@ func validateArgs(args ParsedAddArgs) error {
 	if err != nil {
 		return err
 	}
-	if _, ok := cfg.Catalogs[args.Src]; !ok {
-		return fmt.Errorf("catalog %q not found", args.Src)
+	// check if the catalog file exists
+	if _, err := os.Stat(args.Src); os.IsNotExist(err) {
+		return fmt.Errorf("catalog file %q not found", args.Src)
 	}
-	dst, ok := cfg.Catalogs[args.Dst]
+
+	_, ok := cfg.Catalogs[args.Dst]
 	if !ok {
 		return fmt.Errorf("catalog %q not found", args.Dst)
 	}
-	if dst.URL != "" {
-		return fmt.Errorf("catalog %q is a remote catalog", args.Dst)
+
+	if _, err := os.Stat(args.Src); os.IsNotExist(err) {
+		return fmt.Errorf("source catalog %q not found", args.Dst)
 	}
+
 	if args.Dst == args.Src {
 		return errors.New("cannot add server to the same catalog")
 	}
@@ -77,7 +74,7 @@ func validateArgs(args ParsedAddArgs) error {
 }
 
 func runAdd(args ParsedAddArgs, opts addOpts) error {
-	srcContent, err := ReadCatalogFile(args.Src)
+	srcContent, err := os.ReadFile(args.Src)
 	if err != nil {
 		return err
 	}
@@ -93,7 +90,7 @@ func runAdd(args ParsedAddArgs, opts addOpts) error {
 		return err
 	}
 	dstServerJSON, err := extractServerJSON(dstContentBefore, args.SeverName)
-	if err == nil && len(dstServerJSON) > 0 && !opts.Force {
+	if err == nil && len(dstServerJSON) > 4 && !opts.Force {
 		fmt.Println(string(dstServerJSON))
 		return fmt.Errorf("server %q already exists in catalog %q (use --force to overwrite)", args.SeverName, args.Dst)
 	}
